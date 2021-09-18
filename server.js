a9bce8b096c3136a101dcbda6260cfdf16adc10b
plaban <plabanmondal02@gmail.com>
2021-09-18 19:48:19 +0530

    Add function to save messages in Database

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 
+// importing models
+const Message = require("./models/message");
+
 const DATABASE = process.env.DATABASE
   ? process.env.DATABASE
   : `mongodb://localhost:27017/chat-app-socket-io`;
@@ -25,9 +28,25 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-io.on("connection", (socket) => {});
+io.on("connection", (socket) => {
+  Message.find().then((result) => {
+    socket.emit("old-msg", result);
+  });
+
+  socket.emit("welcome-msg", "Welcome to chat app!");
+
+  // emitting chats
+  socket.on("chatMessage", (msg) => {
+    const message = new Message({ msg });
+    message.save().then(() => {
+      io.emit("chat-msg", msg);
+    });
+  });
+});
 
 const PORT = process.env.PORT || 3000;
+
+// app.listen() doesnt work with socket.io because it creates new server
 server.listen(PORT, () =>
   console.log(`App is running on port ${PORT}`)
 );
